b21973a28bfcef99849f0c58bbd33bc1baf76bdb
jwzzang12 <moonjiwon33@gmail.com>
2025-06-27 16:36:25 +0900

    헤더 배경색 수정 완료

--- js/home.js
+++ js/home.js
@@ -338,3 +338,21 @@ document.addEventListener('DOMContentLoaded', () => {
   updateSlides();
   playCurrentVideo();
 });
+
+//헤더 배경 변경
+const header = document.querySelector('header');
+const hero = document.querySelector('#hero');
+
+if (hero) {
+  window.addEventListener('scroll', () => {
+    const heroBottom = hero.offsetTop + hero.offsetHeight;
+
+    if (window.scrollY < heroBottom) {
+      header.classList.add('blur');
+    } else {
+      header.classList.remove('blur');
+    }
+  });
+} else {
+  header.classList.remove('blur');
+}
